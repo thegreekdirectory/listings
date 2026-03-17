785b38151c3663b5444e571f28dfa18988dfc944
The Greek Directory <thegreekdirectory@gmail.com>
2026-03-17 00:36:33 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // This tells Tailwind to look at all HTML/JS files in your root
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
