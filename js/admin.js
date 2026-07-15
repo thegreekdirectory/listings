5d76436cdfd4a9a6c03714827327d3aaa910c999
The Greek Directory <contact@thegreekdirectory.org>
2026-07-15 01:55:32 -0500

    Add D.C. to US states list

--- js/admin.js
+++ js/admin.js
@@ -120,7 +120,7 @@ let SUBCATEGORIES = {
 
 const US_STATES = {
     'AL': 'Alabama', 'AK': 'Alaska', 'AZ': 'Arizona', 'AR': 'Arkansas', 'CA': 'California',
-    'CO': 'Colorado', 'CT': 'Connecticut', 'DE': 'Delaware', 'FL': 'Florida', 'GA': 'Georgia',
+    'CO': 'Colorado', 'CT': 'Connecticut', 'DC': 'D.C.', 'DE': 'Delaware', 'FL': 'Florida', 'GA': 'Georgia',
     'HI': 'Hawaii', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'IA': 'Iowa',
     'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'ME': 'Maine', 'MD': 'Maryland',
     'MA': 'Massachusetts', 'MI': 'Michigan', 'MN': 'Minnesota', 'MS': 'Mississippi', 'MO': 'Missouri',
