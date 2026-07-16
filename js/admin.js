0b18b9c350cd38494ef316f3f85c6e92e308726a
The Greek Directory <contact@thegreekdirectory.org>
2026-07-16 02:50:13 -0500

    Replace chicagolandCities with chicagolandSuburbs

--- js/admin.js
+++ js/admin.js
@@ -4705,16 +4705,54 @@ async function updateSitemap() {
         };
         
         // Chicago suburbs that should map to "chicagoland"
-        const chicagolandCities = [
-            'oak forest', 'deerfield', 'downers grove', 'oakbrook terrace', 
-            'niles', 'glenview', 'evanston', 'skokie', 'northbrook',
-            'schaumburg', 'naperville', 'aurora', 'joliet', 'elgin',
-            'cicero', 'arlington heights', 'palatine', 'bolingbrook',
-            'des plaines', 'orland park', 'tinley park', 'oak lawn',
-            'berwyn', 'mount prospect', 'wheaton', 'hoffman estates',
-            'oak park', 'buffalo grove', 'bartlett', 'streamwood',
-            'carol stream', 'lombard', 'elmhurst', 'park ridge'
-        ];
+        const chicagolandSuburbs = [
+  "Addison", "Algonquin", "Alsip", "Antioch", "Arlington Heights", "Aurora", 
+  "Bannockburn", "Barrington", "Barrington Hills", "Bartlett", "Batavia", "Beach Park", 
+  "Bedford Park", "Beecher", "Bellwood", "Bensenville", "Berkeley", "Berwyn", 
+  "Bloomingdale", "Blue Island", "Bolingbrook", "Bowdre", "Braidwood", "Bridgeview", 
+  "Broadview", "Brookfield", "Buffalo Grove", "Burbank", "Burnham", "Burr Ridge", 
+  "Calumet City", "Calumet Park", "Campton Hills", "Carol Stream", "Carpentersville", "Cary", 
+  "Cedar Lake", "Channahon", "Chicago Heights", "Chicago Ridge", "Cicero", "Clarendon Hills", 
+  "Country Club Hills", "Countryside", "Crest Hill", "Crestwood", "Crete", "Crystal Lake", 
+  "Darien", "Deer Park", "Deerfield", "Des Plaines", "Diamond", "Dixmoor", 
+  "Dolton", "Downers Grove", "Dyer", "East Chicago", "East Dundee", "East Hazel Crest", 
+  "Elburn", "Elgin", "Elk Grove Village", "Elmhurst", "Elmwood Park", "Evanston", 
+  "Evergreen Park", "Flossmoor", "Ford Heights", "Forest Park", "Forest View", "Fox Lake", 
+  "Fox River Grove", "Frankfort", "Franklin Park", "Gary", "Geneva", "Gilberts", 
+  "Glen Ellyn", "Glencoe", "Glendale Heights", "Glenview", "Glenwood", "Godley", 
+  "Golf", "Grayslake", "Green Oaks", "Gurnee", "Hammond", "Hainesville", 
+  "Hampshire", "Hanover Park", "Harvard", "Harvey", "Harwood Heights", "Hawthorn Woods", 
+  "Hazel Crest", "Hebron", "Hickory Hills", "Highland", "Highland Park", "Highwood", 
+  "Hillside", "Hinsdale", "Hodgkins", "Hoffman Estates", "Hometown", "Homewood", 
+  "Huntley", "Indian Head Park", "Inverness", "Island Lake", "Itasca", "Johnsburg", 
+  "Joliet", "Justice", "Kankakee", "Kenilworth", "Kildeer", "La Grange", 
+  "La Grange Park", "Lake Barrington", "Lake Blossom", "Lake Bluff", "Lake Forest", "Lake In The Hills", 
+  "Lake Station", "Lake Villa", "Lake Zurich", "Lakemoor", "Lakewood", "Lansing", 
+  "Lemont", "Libertyville", "Lily Lake", "Lincolnshire", "Lincolnwood", "Lindenhurst", 
+  "Lisbon", "Lisle", "Lockport", "Lombard", "Long Grove", "Lynwood", 
+  "Lyons", "Manhattan", "Maple Park", "Marengo", "Markham", "Matteson", 
+  "Maywood", "McCook", "McCullom Lake", "McHenry", "Melrose Park", "Merrillville", 
+  "Merrionette Park", "Midlothian", "Minooka", "Mokena", "Monee", "Montgomery", 
+  "Morris", "Morton Grove", "Mount Prospect", "Mundelein", "Munster", "Naperville", 
+  "New Lenox", "Niles", "Norridge", "North Aurora", "North Chicago", "North Riverside", 
+  "Northbrook", "Northfield", "Northlake", "Oak Brook", "Oak Forest", "Oak Lawn", 
+  "Oak Park", "Oakwood Hills", "Olympia Fields", "Orland Hills", "Orland Park", "Oswego", 
+  "Palatine", "Palos Heights", "Palos Hills", "Palos Park", "Park City", "Park Forest", 
+  "Park Ridge", "Peotone", "Phoenix", "Pingree Grove", "Plainfield", "Posen", 
+  "Prairie Grove", "Prospect Heights", "Richmond", "Richton Park", "Ringwood", "River Forest", 
+  "River Grove", "Riverdale", "Riverside", "Riverwoods", "Robbins", "Rockdale", 
+  "Rolling Meadows", "Romeoville", "Roselle", "Rosemont", "Round Lake", "Round Lake Beach", 
+  "Round Lake Heights", "Round Lake Park", "Sauk Village", "Schaumburg", "Schiller Park", "Schererville", 
+  "Shorewood", "Skokie", "Sleepy Hollow", "South Barrington", "South Chicago Heights", "South Elgin", 
+  "South Holland", "Spring Grove", "St. Charles", "Steger", "Stickney", "Stone Park", 
+  "Streamwood", "Sugar Grove", "Summit", "Symphony Center", "Symerton", "Third Lake", 
+  "Thornton", "Tinley Park", "Tower Lakes", "Trout Valley", "Turnberry", "University Park", 
+  "Valparaiso", "Vernon Hills", "Villa Park", "Volo", "Wadsworth", "Warrenville", 
+  "Wauconda", "Waukegan", "Wayne", "West Chicago", "West Dundee", "Westchester", 
+  "Western Springs", "Westmont", "Wheaton", "Wheeling", "Whiting", "Willow Springs", 
+  "Willowbrook", "Wilmette", "Wilmington", "Winfield", "Winthrop Harbor", "Winnetka", 
+  "Wood Dale", "Woodridge", "Woodstock", "Worth", "Yorkville", "Zion"
+];
         
         database.listings.forEach(listing => {
             if (listing.visible !== false && listing.city && listing.state && (listing.country || 'USA') === 'USA') {
